75afa9500dd27b4b31eb4ce513e6c3ca1e04364d
YoLetsGitIt <YoLetsGitIt@github.com>
2022-09-23 23:13:35 +1000

    completed home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 
 import LinkedIn from '../assets/img/linkedin.svg'
 import Github from '../assets/img/github.svg'
@@ -10,6 +12,20 @@ import Socials from '../components/Socials'
 
 export default function Home() {
 
+  const LogoAnimation = () => {
+    const mesh = useRef(null)
+    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.002))
+  
+    return (
+      <>
+        <mesh ref={mesh}>
+          <sphereGeometry args={[1, 40, 40]} />
+          <meshStandardMaterial attach='material' color='#143D66' wireframe={true} />
+        </mesh>
+      </>
+    )
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.profile}>
@@ -19,8 +35,12 @@ export default function Home() {
         <h1>a <span>Front-end Developer</span> and <span>UX Designer</span> in <span>Melbourne</span></h1>
         <Socials/>
       </div>
-      <div>
-
+      <div className={styles.animation}>
+        <Canvas shadows camera={{ position: [-5, 2, 10], fov: 20 }}>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 15, 10]} angle={0.3} castShadow />
+          <LogoAnimation />
+        </Canvas>
       </div>
     </div>
   )
